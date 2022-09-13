077c3e824edf2fbfaa66f16c0939f60e8cb7b984
Priscila Jauregui <>
2022-09-13 14:40:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+print("Bienvenido a mi trivia sobre videojuegos")
+print("Pondremos a prueba tus conocimientos")
+
+print(
+    "Responder las siguientes preguntas escribiendo la letra de la alternativa que creas correcta y presionando Enter para enviar tu respuesta:\n"
+)
+
+print("1) ¿Cual es el videojuego mas antiguo?")
+print("a) Arcade")
+print("b) oxo")
+print("c) Counter strike")
+print("d) Undertale")
+
+respuesta_1 = input("Tu respuesta: ")
+
+while respuesta_1 not in ("a","b","c","d"):
+  respuesta_1 = input("Debes responder a, b,c o d. Ingresa nuevamente tu respuesta: ")
+
+  
+if respuesta_1 == "a":
+  print("Incorrecto!", nombre ,"Arcade no es el videojuego mas antiguo")
+
+
+else respuesta_1 == "b":
+print("Muy bien ",nombre, "oxo es el videojuego mas antiguo")
+elif respuesta_1 == "c":
+print("Incorrecto!",nombre,"Counter- strike no es el videojuego mas antiguo")
+elif respuesta_1 == "d":
+print("Incorrecto!",nombre ,"Undertale no es el videojuego mas antiguo")
+
+
+  
+  #pregunta 2
+  print("2) ¿Cual es el videojuego top 1?")
+    print("a) Tetris")
+    print("b) Minecraft")
+    print("c) Mario Bros")
+    print("d) GTA")
+
+    Respuesta_2 = input("Tu respuesta:")
+
+   while respuesta_2 not in ("a", "b", "c", "d"):
+     respuesta_2 =
+     input("Debes responder a,b,c o d.Ingresa nuevamente tu respuesta:)
+
+
+ if respuesta_2 == "a":
+     print("Incorrecto!",nombre ,"Tetris no es el videojuego top 1")
+     else respuesta_2 == "b":
+     print("Muy bien", nombre, "Minecraft es el videojuego top 1")
+elif respuesta_2 == "c":
+print("Incorrecto!",nombre, "Mario Bros no es el videojuego top 1")
+elif respuesta_2 == "d":
+print("Incorrecto!",nombre, "GTA no es el videojuego top 1")
+
+           
+  #pregunta 3
+        print("3)¿Cual es el videojuego mas actualizado?")
+        print("a) Elden ring")
+        print("b) Fre Fire")
+        print("c) Among us")
+        print("d) Fortnite")
+
+        Respuesta_3 = input("Tu respuesta:")
+
+        while respuesta_3 not in ( "a", "b","c","d") :
+          respuesta_3 =
+          input("Debes responder a, b, c o d.Ingresa nuevamente tu respuesta: ") 
+          
+          
+else  respuesta_3 == "a":
+  print("Muy bien",nombre,"Elden Ring es el videojuego mas actualizado")
+elif respuesta_3 == "b":
+print("Incorrecto¡", nombre,"Free Fire no es el videojuego mas actualizado")
+elif respuesta_3 == "c":"Among us no es el videojuego mas actualizado")
+if respuesta_3 == "d":
+print("Incorrecto¡",nombre, "Fornite no es el video mas actualizado")
